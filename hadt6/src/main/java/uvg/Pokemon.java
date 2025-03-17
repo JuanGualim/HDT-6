7541406e179fca010d68d8fa8927a62be8548eef
JuanGualim <gua24852@uvg.edu.gt>
2025-03-16 21:59:58 -0600

    Creación de la clase pokemon

---
+++ hadt6/src/main/java/uvg/Pokemon.java
@@ -0,0 +1,112 @@
+package uvg;
+
+import java.util.List;
+
+public class Pokemon {
+    private String name, type1, type2, classification;
+    private int pokedexNumber, generation;
+    private double height, weight;
+    private boolean isLegendary;
+    private List<String> abilities;
+
+    public Pokemon(String name, int pokedexNumber, String type1, String type2, String classification, double height, double weight, List<String> abilities, int generation, boolean isLegendary) {
+        this.name = name;
+        this.pokedexNumber = pokedexNumber;
+        this.type1 = type1;
+        this.type2 = type2;
+        this.classification = classification;
+        this.height = height;
+        this.weight = weight;
+        this.abilities = abilities;
+        this.generation = generation;
+        this.isLegendary = isLegendary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType1() {
+        return type1;
+    }
+
+    public void setType1(String type1) {
+        this.type1 = type1;
+    }
+
+    public String getType2() {
+        return type2;
+    }
+
+    public void setType2(String type2) {
+        this.type2 = type2;
+    }
+
+    public String getClassification() {
+        return classification;
+    }
+
+    public void setClassification(String classification) {
+        this.classification = classification;
+    }
+
+    public int getPokedexNumber() {
+        return pokedexNumber;
+    }
+
+    public void setPokedexNumber(int pokedexNumber) {
+        this.pokedexNumber = pokedexNumber;
+    }
+
+    public int getGeneration() {
+        return generation;
+    }
+
+    public void setGeneration(int generation) {
+        this.generation = generation;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public boolean isLegendary() {
+        return isLegendary;
+    }
+
+    public void setLegendary(boolean isLegendary) {
+        this.isLegendary = isLegendary;
+    }
+
+    public List<String> getAbilities() {
+        return abilities;
+    }
+
+    public void setAbilities(List<String> abilities) {
+        this.abilities = abilities;
+    }
+
+    @Override
+    public String toString() {
+        return "Pokemon: \nname=" + name + "\ntype1=" + type1 + "\ntype2=" + type2 + "\nclassification=" + classification
+                + "\npokedexNumber=" + pokedexNumber + "\ngeneration=" + generation + "\nheight=" + height + "\nweight="
+                + weight + "\nisLegendary=" + isLegendary + "\nabilities=" + abilities + "";
+    }
+
+}
